d9eb93a246ffbf012537131a08711e1f78b18896
AngkorW <AngkorW@ANGKORW-DEV>
2023-06-30 15:32:49 +0800

    Command="{x:Bind ShowCommand}"

---
+++ samples/MvvmSample.Core/Models/ThumbnailItem.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Threading.Tasks;
+
+namespace MvvmSample.Core.Models
+{
+    public partial class ThumbnailItem : ObservableObject
+    {
+        public ThumbnailItem()
+        {
+        }
+        public string Name { get; set; }
+
+        public Picture Picture { get; set; }
+
+        public string Email { get; set; }
+
+
+        [RelayCommand]
+        public async Task ShowAsync()
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
--- samples/MvvmSample.Core/ViewModels/ThumbnailViewModel.cs
+++ samples/MvvmSample.Core/ViewModels/ThumbnailViewModel.cs
@@ -1,12 +1,53 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Mvvm.Collections;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MvvmSample.Core.Models;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace MvvmSample.Core.ViewModels
 {
-    public class ThumbnailViewModel : ObservableObject
+    public partial class ThumbnailViewModel : ObservableObject
     {
         public ThumbnailViewModel()
         {
+            Items = new ObservableGroupedCollection<string, ThumbnailItem>();
+            foreach (var item in Enumerable.Range(0, 100))
+            {
+                var group = new ObservableGroup<string, ThumbnailItem>($"{item}") { new ThumbnailItem() { Name = $"{item * item}" } };
+                Items.Add(group);
+            }
+        }
 
+        public ObservableGroupedCollection<string, ThumbnailItem> Items { get; private set; }
+
+        [RelayCommand]
+        public async Task GetMoreItemsAsync()
+        {
+            await Task.CompletedTask;
+        }
+
+        [RelayCommand]
+        private async Task ViewAsync()
+        {
+            await Task.CompletedTask;
+        }
+
+        [RelayCommand]
+        private async Task EditAsync()
+        {
+            await Task.CompletedTask;
+        }
+
+        [RelayCommand]
+        private async Task DeleteAsync()
+        {
+            await Task.CompletedTask;
+        }
+        [RelayCommand]
+        private async Task AddAsync()
+        {
+            await Task.CompletedTask;
         }
     }
 }
